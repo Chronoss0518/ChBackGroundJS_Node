11652a7ba6db41b9e7a7c36e80ceedd75d8dec1e
Chronoss0518 <chronoss@chworld.org>
2022-06-21 18:02:57 +0900

    first commit

---
+++ ChJSLibrary.js
@@ -0,0 +1,40 @@
+
+    const types = 
+    {
+        '.html' : 'text/html',
+        '.htm'  : 'text/html',
+        '.xml'  : 'text/xml',
+        '.xaml' : 'text/xml',
+        '.css'  : 'text/css',
+        '.js'   : 'text/javascript',
+        '.vbs'  : 'text/vbscript',
+        '.png'  : 'image/png',
+        '.jpg'  : 'image/jpeg',
+        '.jpeg' : 'image/jpeg',
+        '.gif'  : 'image/gif',
+        '.svg'  : 'image/svg+xml',
+        '.ico'  : 'image/vnd.microsoft.icon',
+        '.mp3'  : 'audio/mpeg',
+        '.mp4'  : 'video/mp4',
+        '.json' : 'text/json'
+    };
+    
+    exports.GetMimeType = function(_url) 
+    {
+        for (var key in types) {
+          if (_url.endsWith(key)) {
+            return types[key];
+          }
+        }
+        return 'text/plane';
+    };
+
+    exports.IsMimeType = function(_url) 
+    {
+        for (var key in types) {
+          if (_url.endsWith(key)) {
+            return true;
+          }
+        }
+        return false;
+    };
---
+++ startLocalHost.js
@@ -0,0 +1,65 @@
+
+
+
+const chlib = require('./ChJSLibrary')
+
+// Node.js の http モジュールを読み込む
+const http = require('http');
+const file = require('fs');
+
+const firstHTML = process.argv.length > 2 ? process.argv[2] : "../index.html";
+
+// サーバーを生成
+let myServer = http.createServer();
+
+console.log("start");
+
+function RequestFunction(req, res){
+    // アクセス情報をターミナルに出力
+    var url = (req.url.indexOf('.') < 0 ? __dirname + req.url + firstHTML :  __dirname + req.url );
+    //if(!file.existsSync(url)){url = 'public' + url;}
+
+    console.log(`url:${url}`);
+    console.log(`method:${req.method}`);
+    //console.log('load html:' + __dirname + "/" + firstHTML);
+
+    const mimeType = chlib.GetMimeType(url);
+    console.log("mimeType:" + mimeType);
+
+    console.log(file.existsSync(url) ? "Load True" : "Load False");
+    
+    var cordding = (mimeType.split("/")[0] == "text" ? "utf-8" : "Binary");
+    console.log(cordding);
+    
+
+
+        file.readFile(url,cordding , function(err, data)
+        {
+            // ファイル読み込みエラーが発生した場合
+            if (err)
+            {
+                // ファイルが存在しない場合のHTTPステータスコード「404」を、HTTPヘッダに入れる
+                // 文字を表示するため「text/plain」に設定する
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                // 本文（Body部）に文字を表示する
+                res.write('Page Not Found!');
+                return res.end();
+            }
+            
+            // 成功した場合のHTTPステータスコード「200」を、HTTPヘッダに入れる
+            // HTMLを表示するため「text/html」に設定する
+            res.writeHead(200, {'Content-Type': mimeType});
+            // dataに読み込んだファイル内容が入っているので、そのまま表示する
+            
+            res.end(data,cordding);
+        });
+
+
+};
+
+myServer.on('request',RequestFunction); 
+
+myServer.listen(port = 8000);
+
+
+console.log("wait");
